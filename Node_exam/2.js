0c2dc99ba10732868097c23c78bc72713b5fb062
Grzegorz Smoktunowicz <biarth@wp.pl>
2022-01-02 21:45:44 +0100

    new commit

--- Node_exam/2.js
+++
@@ -1,8 +0,0 @@
-const colors = require('colors');
-const text = process.argv[2];
-if(text){
-    console.log(text.rainbow);
-}else{
-    throw new Error('Podaj argument!');
-};
-// Przykładowe wywołanie: node 2.js hello
