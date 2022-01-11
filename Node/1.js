79cbc3c5e6d8942dd33c54a5f14d0697e0a520f1
Grzegorz Smoktunowicz <biarth@wp.pl>
2022-01-11 12:54:29 +0100

    new commit

--- Node/1.js
+++
@@ -1,4 +0,0 @@
-const func = () => {
-  func();
-};
-console.log(func());
--- Node/3.js
+++
@@ -1,9 +0,0 @@
-let fs = require('fs');
-
-fs.stat(__filename, (error, stat) => {
-    if(error){
-        console.log(error);
-    }else{
-        console.log(stat.size, stat.atime, stat.mtime);
-    };
-  });
