805c83e0ac221254be433e3564be0115200f26b8
gitpb <92650386+Gsmokt@users.noreply.github.com>
2022-01-13 22:31:39 +0100

    Update ToDo.js

--- Node_Exam/6/ToDo.js
+++ Node_Exam/6/ToDo.js
@@ -1,16 +1,20 @@
 const fs = require('fs');
 const file = './fil.json';
-const storeData = (data, path) => {
+function storeData (data, path){
   const er = new Error('Nie udało się dodać nowego zadania');
-try {
+  if(!data){
+    console.log('Wpisz zadanie')
+  }else{
+      try {
   const fl = JSON.parse(fs.readFileSync(path));
   const ob = {task: data};
   fl.push(ob);
-  
+        
     fs.writeFileSync(path, JSON.stringify(fl));
     console.log('Dodałeś nowe zadanie!');
   } catch (error) {
     console.log(er.message);
+  }
   }
 }
 module.exports={
