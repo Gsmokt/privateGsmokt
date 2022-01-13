0cf76fcfbc3f58aba18da91b2f469d95b80eacc7
gitpb <92650386+Gsmokt@users.noreply.github.com>
2022-01-13 19:33:09 +0100

    Update DeleteTask.js

--- Node_Exam/6/DeleteTask.js
+++ Node_Exam/6/DeleteTask.js
@@ -4,10 +4,13 @@ const deleteTask = (number, path) => {
   const er = new Error('Nie udało się usunąć zadania');
     try {
     const fl = JSON.parse(fs.readFileSync(path));
-    fl.splice(number - 1,1);
-   
-        fs.writeFileSync(path, JSON.stringify(fl));
-        console.log('Usunąłeś zadanie!');
+    if(fl.length <= 0){
+      console.log('Lista jest pusta, nie możesz nic usunąć')
+    }else{    
+      fl.splice(number - 1,1);
+      fs.writeFileSync(path, JSON.stringify(fl));
+      console.log('Usunąłeś zadanie!');
+    }
       } catch (err) {
         console.log(er.message);
       };
