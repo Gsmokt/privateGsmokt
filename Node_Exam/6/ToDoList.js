ec771abf997d6a47d26e06050ef9650eb73f4c48
gitpb <92650386+Gsmokt@users.noreply.github.com>
2022-01-14 21:21:01 +0100

    Update ToDoList.js

--- Node_Exam/6/ToDoList.js
+++ Node_Exam/6/ToDoList.js
@@ -7,6 +7,7 @@ const printData = (path) => {
     if(fl.length === 0){
         console.log(`Aktualnie lista jest pusta. Możesz dodać nowe zadanie`);
     }else{
+            console.log('Twoja lista zadań:');
             fl.map((e,i) => {
             console.log(`${i+1} - ${e.task}`);
             })
