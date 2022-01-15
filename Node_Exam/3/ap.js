3e362208d7944360b704a76c1262b33ec1598cc7
gitpb <92650386+Gsmokt@users.noreply.github.com>
2022-01-15 22:37:28 +0100

    Update ap.js

--- Node_Exam/3/ap.js
+++ Node_Exam/3/ap.js
@@ -2,7 +2,6 @@ let fs = require('fs');
 
 
 const func = (filename) => {
-const er = new Error('Nie udało się pobrać danych');
 try{
     const stat = fs.statSync(filename);
     console.log(`File size: ${stat.size} B`);
@@ -10,6 +9,7 @@ try{
     console.log(`File data modification date : ${stat.mtime.toLocaleString()}`);
     console.log(`File status modification date: ${stat.ctime.toLocaleString()}`);
   }catch(error){
+    const er = new Error('Nie udało się pobrać danych');
     console.log(er.message);
   }
 }
