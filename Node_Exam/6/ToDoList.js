630b7c04030144ec3a43a6532cf5181833ce770c
gitpb <92650386+Gsmokt@users.noreply.github.com>
2022-01-17 14:52:14 +0100

    Update ToDoList.js

--- Node_Exam/6/ToDoList.js
+++ Node_Exam/6/ToDoList.js
@@ -1,21 +1,28 @@
 const fs = require('fs');
+const util = require('util');
+const readAsync = util.promisify(fs.readFile);
 
-const printData = (path) => {
-    try{   
-    const fl = JSON.parse(fs.readFileSync(path));
-    if(fl.length === 0){
-        console.log(`Aktualnie lista jest pusta. Możesz dodać nowe zadanie`);
-    }else{
-            console.log('Twoja lista zadań:');
-            fl.map((e,i) => {
-            console.log(`${i+1} - ${e.task}`);
-            })
-        } 
-    }catch(error){
-    const er = new Error('Nie udało się wyświetlić listy');
-    console.log(er.message);
-    }
-}
+  const read = async(path) => {
+    const file = await readAsync(path, {encoding:'utf-8'});
+    return file;
+  }
+
+  const printData = async(path) => {
+      try{
+        const file = await read(path);
+        const fl = JSON.parse(file)
+        if(fl.length === 0){
+                  console.log(`Aktualnie lista jest pusta. Możesz dodać nowe zadanie`);
+              }else{
+                      console.log('Twoja lista zadań:');
+                      fl.map((e,i) => {
+                      console.log(`${i+1} - ${e.task}`);
+                      })
+                  } 
+      }catch(error){
+        console.log(new Error('Nie udało się wyświetlić listy'));
+      }
+  }
 
 module.exports = {
     printData
