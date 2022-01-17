b58ced15dfc23a03dc10f19a22e2bb3b3bc3ab67
gitpb <92650386+Gsmokt@users.noreply.github.com>
2022-01-17 14:48:59 +0100

    Update main.js

--- Node_Exam/5/main.js
+++ Node_Exam/5/main.js
@@ -8,30 +8,22 @@ async function api(name,followers){
     const userr = await user(name);
     const weatherr = await weather(userr);
     const rep = await repos(name);
-    if(followers === undefined || null || followers !== 'followers'){
-        const repo = rep.data.map((e,i) => `${i+1}.${e.name}\n`);
-        const reposit = repo.join("");
-        const obj ={
+    const repo = rep.data.map((e,i) => `${i+1}.${e.name}\n`);
+    const reposit = repo.join("");
+    const obj ={
             Username: userr.data.name,
             Repos: userr.data.public_repos,
             Weather: `${weatherr.data.weather[0].main} and ${weatherr.data.weather[0].description}`,
             Repos_names: reposit
         };
+    if(followers === undefined || null || followers !== 'followers'){
         for(const [key,value] of Object.entries(obj)){
             console.log(`${key}: ${value}`)
         }
         console.log('Aby wyświetlić liczbę followersów, wpisz: followers, po nazwie użytkownika');
     }else{
-        const repo = rep.data.map((e,i) => `${i+1}.${e.name}\n`);
-        const reposit = repo.join("");
-        const obj ={
-            Username: userr.data.name,
-            Followers: userr.data.followers,
-            Repos: userr.data.public_repos,
-            Weather: `${weatherr.data.weather[0].main} and ${weatherr.data.weather[0].description}`,
-            Repos_names: reposit
-        };
-        for(const [key,value] of Object.entries(obj)){
+        const newObj = {Followers: userr.data.followers, ...obj}
+        for(const [key,value] of Object.entries(newObj)){
             console.log(`${key}: ${value}`);
         }
         }
