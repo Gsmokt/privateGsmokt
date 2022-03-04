07ba4e283414ce74250225f5318c7133bc0a7bc9
Grzegorz Smoktunowicz <biarth@wp.pl>
2022-03-04 13:57:26 +0100

    new commit

--- Js_Homework/2.js
+++ Js_Homework/2.js
@@ -1,15 +1,18 @@
-// const digits = (...param) => {
-//     console.log(param)
-//   console.log(param.filter(el => typeof el === 'number'));
-// };
-
-// digits('A,2,3,4,2')
-
 const game = (num) => {
   const arr = [];
   arr.push(num);
-  return function(){
-    console.log(arr)
+  function nam(arg){
+    if(arg < 1 || arg > 100) return console.log('Enter a number from the range: 1 - 100');
+    if(arg === num) return console.log(`You won! Needed ${arr.length} tries`);
+    if(arr.includes(arg)) return console.log('You tried this number');
+    arr.push(arg);
+    if(arg < num) return console.log('Too small');
+    if(arg > num) return console.log('Too large');
+    return;
   }
+  return nam;
 }
-const 
+const gam = game(Math.floor(Math.random()*100)+1);
+console.log(gam());
+
+
