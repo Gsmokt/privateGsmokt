d777b5afbf75fa29087cfcaf3fa7d05c0735a905
gitpb <92650386+Gsmokt@users.noreply.github.com>
2022-03-04 14:45:50 +0100

    Update 2.js

--- Js_Homework/2.js
+++ Js_Homework/2.js
@@ -2,6 +2,7 @@ const game = (num) => {
   const arr = [];
   arr.push(num);
   function nam(arg){
+    if(!arg) return console.log('Enter a number');
     if(arg < 1 || arg > 100) return console.log('Enter a number from the range: 1 - 100');
     if(arg === num) return console.log(`You won! Needed ${arr.length} tries`);
     if(arr.includes(arg)) return console.log('You tried this number');
