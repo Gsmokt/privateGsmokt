409b01432522e5282025cd3d97cebb43b2dff7b9
gitpb <92650386+Gsmokt@users.noreply.github.com>
2022-03-05 15:20:23 +0100

    Update 5.js

--- Js_Homework/5.js
+++ Js_Homework/5.js
@@ -1,8 +1,8 @@
 
 const numbersFilter = (arg) => {
-  if(typeof arg === 'number') console.log(arg)
+  if(typeof arg === 'number') console.log(arg);
   else if(typeof arg === 'string') console.log(parseInt(arg.match(/\d/g).join(''), 10));
   else return;
-};
+}
 
 numbersFilter('aFFF,,,c2f3g42');
